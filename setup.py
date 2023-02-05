4926b7ca4f1f333d0b846caaf2bd26778e8c6b6e
Charl P. Botha <cpbotha@vxlabs.com>
2023-02-05 19:53:35 +0200

    Adapt setup to lttbcy.pyx

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from Cython.Build import cythonize
 extensions = [
     # only with Cython 3 and up, define NPY_* to avoid the OLD API
     # https://cython.readthedocs.io/en/latest/src/userguide/source_files_and_compilation.html#configuring-the-c-build    
-    Extension("lttbcy", ["lttb.pyx"], define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")])
+    Extension("lttbcy", ["lttbcy.pyx"], define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")])
 ]
 
 setup(
